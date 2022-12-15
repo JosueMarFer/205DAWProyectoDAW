e2d9ca7b0cc11ceaeaab6b9c7fd69bf2cdeb888e
ZenOne <ZenOne@DESKTOP-V49KOCS>
2022-12-15 05:27:14 +0100

    15/12/2022
    1.0

---
+++ indexProyectoDAW.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./webroot/css/style.css">
+    <title>205ProyectoDAW</title>
+  </head>
+
+  <body>
+    <header>
+      <h1>&lt;/DAW&gt;</h1>
+      <h2>Despliegue de Aplicaciones Web</h2>
+    </header>
+    <main>
+      <section>   
+        <a href="./doc/T1.pdf" target="_blank">
+          <h3>TEMA 1. Implantacion de aplicaciones web</h3>
+        </a>
+        <a href="./doc/T2.pdf" target="_blank">
+          <h3>TEMA 2. Administracion de servidores web</h3>
+        </a>
+        <a href="./doc/T3.pdf" target="_blank">
+          <h3>TEMA 3. Servicios de transferencia de archivos</h3>
+        </a>
+        <a href="./doc/T4.pdf" target="_blank">
+          <h3>TEMA 4. DNS</h3>
+        </a>
+        <a href="./doc/T5.pdf" target="_blank">
+          <h3>TEMA 5. Control de versiones</h3>
+        </a>
+        <a href="./doc/T6.pdf" target="_blank">
+          <h3>TEMA 6. Servidor de aplicaciones</h3>
+        </a>
+        <a href="./doc/T7.pdf" target="_blank">
+          <h3>TEMA 7. Documentación</h3>
+        </a>
+        <a href="./doc/T8.pdf" target="_blank">
+          <h3>TEMA 6. Servicio LDAP</h3>
+        </a>
+        <a href="./doc/T9.pdf" target="_blank">
+          <h3>TEMA 6. Docker</h3>
+        </a>
+      </section>
+      <div class="return">
+        <a href="../index.html"><img src="./webroot/images/back.png" alt="Imagen back"></a>
+      </div>
+    </main>
+    <footer>
+      <div class="footerIcons">
+        <a href="./doc/curriculum.pdf" target="_blank"><img src="./webroot/images/curriculum.png" alt="Imagen curriculum"></a>
+        <a href="https://github.com/JosueMarFer/205DAWProyectoDAW" target="_blank"><img src="./webroot/images/github.png"
+                                                                                          alt="Imagen github"></a>
+      </div>
+      <div class="home">
+        <a href="../index.html"><img src="./webroot/images/home.png" alt="Imagen home"></a>
+        <p>Josué martínez Fernández</p>
+      </div>
+    </footer>
+  </body>
+
+</html>
